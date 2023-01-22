ec34c7b0dc8c2f6d994827706a5498a9234785bd
Amitesh Patnaik <49634729+amitesh03@users.noreply.github.com>
2023-01-22 23:20:50 +0530

    added notes input component

--- src/Bookmarks/Bookmarks.js
+++ src/Bookmarks/Bookmarks.js
@@ -1,7 +1,7 @@
 import styles from "./Bookmarks.module.css";
 
 export default function Bookmarks(props){
-    const link = `https://${props.text}`;
+    const link = `${props.text}`;
     return (
         <div className={styles.container}>
             <a href={link} target="_blank">{props.text}</a>
